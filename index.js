38a83ccb5f1a168cd46ea8fd3abf64c120e7d400
Jurzenas <62443398+Jurzenas@users.noreply.github.com>
2020-04-20 19:25:55 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-//alert("Working");
-var diceR = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"];
 
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
-var diceR1 = "images/dice" + randomNumber1 + ".png";  //images/dice6.png
-document.querySelectorAll("img")[0].setAttribute("src", diceR1);
 
-var randomNumber2 = Math.random();
-randomNumber2 = Math.floor(randomNumber2 * 6);
-diceR2 = diceR[randomNumber2];
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
-document.querySelectorAll("img")[1].setAttribute("src", diceR2);
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
 
-document.getElementsByTagName("h1")[0].innerHTML = PlayerRefresh();
+var image1 = document.querySelectorAll("img")[0];
 
-function PlayerRefresh() {
-  if(randomNumber1 > randomNumber2) {
-return "🚩Player 1 Wings!";
-  } else if(randomNumber1 < randomNumber2) {
-return "Player 2 Wings!🚩";
-  } else  {
-return "Draw!";
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
 }
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
 }
